776c14c702940d86252d67726fb723e012377ef7
Pukar Chhatkuli <22.student.pukar@broadwayinfosys.edu.np>
2023-03-17 15:35:37 +0545

    4:23:13

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,7 +15,7 @@ import Genres from "../genres/Genres";
 
 import "./style.scss";
 
-const Carousel = ({ data, loading }) => {
+const Carousel = ({ data, loading, endpoint }) => {
     const carouselContainer = useRef();
     const {url} = useSelector((state) => state.home);
     const navigate = useNavigate();
@@ -57,7 +57,7 @@ const Carousel = ({ data, loading }) => {
                         {data?.map((item) => {
                             const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
                             return (
-                                <div key={item.id} className="carouselItem">
+                                <div key={item.id} className="carouselItem" onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}>
                                     <div className="posterBlock">
                                         <Img src={posterUrl} alt="" />
                                         <CircleRating rating={item.vote_average.toFixed(1)} />
