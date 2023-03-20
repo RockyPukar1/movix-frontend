4be4e93963afe8cc1f049138960b9a12c3177bfd
Pukar Chhatkuli <22.student.pukar@broadwayinfosys.edu.np>
2023-03-20 14:00:07 +0545

    6:09:11

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection"
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation"
 
 import "./style.scss";
 const Details = () => {
@@ -16,6 +19,9 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
